ff8b363555128684350bee0a21df2a8219a635bf
arthur-ryoo <jene0523@gmail.com>
2020-08-16 21:24:54 -0700

    Import axios and two APIs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+
+const FIXER_API = `http://data.fixer.io/api/latest?access_key=${process.env.REACT_APP_FIXER_API_KEY}`;
+const REST_CONTRIES_API = `https://restcountries.eu/rest/v2/currency/cop`;
+
 function App() {
   return (
     <div className="App">
